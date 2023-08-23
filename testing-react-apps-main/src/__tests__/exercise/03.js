8e7ed5a4cd137aa666bc62bbad99239c6a9cb014
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2023-08-23 22:33:04 +0100

    fin part 4

--- testing-react-apps-main/src/__tests__/exercise/03.js
+++ testing-react-apps-main/src/__tests__/exercise/03.js
@@ -1,21 +1,23 @@
-// Avoid implementation details
-// http://localhost:3000/counter
-
 import * as React from 'react'
-// 🐨 add `screen` to the import here:
-import {render, fireEvent} from '@testing-library/react'
+import {render, screen} from '@testing-library/react'
 import Counter from '../../components/counter'
+import userEvent from "@testing-library/user-event"
 
-test('counter increments and decrements when the buttons are clicked', () => {
-  const {container} = render(<Counter />)
-  // 🐨 replace these with screen queries
-  // 💰 you can use `getByText` for each of these (`getByRole` can work for the button too)
-  const [decrement, increment] = container.querySelectorAll('button')
-  const message = container.firstChild.querySelector('div')
+test('counter increments and decrements when the buttons are clicked', async () => {
+  // userEvent is another method that allows us to overcome implementation biases
+  const user = userEvent.setup()
+  render(<Counter />)
 
+  // This method allows us to avoid issues with implementation details
+  // Regex makes it case insensitive
+  const increment = screen.getByRole("button", { name: /increment/i });
+  const decrement = screen.getByRole("button", { name: /decrement/i });
+  const message = screen.getByText(/current count/i);
+
+  // Click is actually a range of hover over, mouse down, mouse up, hover out
   expect(message).toHaveTextContent('Current count: 0')
-  fireEvent.click(increment)
+  await user.click(increment)
   expect(message).toHaveTextContent('Current count: 1')
-  fireEvent.click(decrement)
+  await user.click(decrement)
   expect(message).toHaveTextContent('Current count: 0')
 })
--- testing-react-apps-main/src/__tests__/exercise/04.js
+++ testing-react-apps-main/src/__tests__/exercise/04.js
@@ -1,30 +1,25 @@
-// form testing
-// http://localhost:3000/login
-
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
 
-test('submitting the form calls onSubmit with username and password', () => {
-  // 🐨 create a variable called "submittedData" and a handleSubmit function that
-  // accepts the data and assigns submittedData to the data that was submitted
-  // 💰 if you need a hand, here's what the handleSubmit function should do:
-  // const handleSubmit = data => (submittedData = data)
-  //
-  // 🐨 render the login with your handleSubmit function as the onSubmit prop
-  //
-  // 🐨 get the username and password fields via `getByLabelText`
-  // 🐨 use `await userEvent.type...` to change the username and password fields to
-  //    whatever you want
-  //
-  // 🐨 click on the button with the text "Submit"
-  //
-  // assert that submittedData is correct
-  // 💰 use `toEqual` from Jest: 📜 https://jestjs.io/docs/en/expect#toequalvalue
-})
+test('submitting the form calls onSubmit with username and password', async () => {
+  const user = userEvent.setup()
 
-/*
-eslint
-  no-unused-vars: "off",
-*/
+  let submittedData;
+  const handleSubmit = (data) => (submittedData = data)
+  render(<Login onSubmit={handleSubmit} />)
+
+  const username = "Just my name";
+  const password = "No password";
+
+  const usernameInput = screen.getByLabelText(/username/i);
+  const passwordInput = screen.getByLabelText(/password/i);
+  const submitButton = screen.getByRole("button", { name: /submit/i });
+  
+  await user.type(usernameInput, username)
+  await user.type(passwordInput, password)
+  await user.click(submitButton)
+
+  expect(submittedData).toEqual({ username, password })
+})
