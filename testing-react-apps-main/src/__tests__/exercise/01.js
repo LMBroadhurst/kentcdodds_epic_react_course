7fe69976eac32d51a6c03d4660fa8dd8fc6a27dd
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2023-08-23 20:51:54 +0100

    finished exercise 2

--- testing-react-apps-main/src/__tests__/exercise/01.js
+++ testing-react-apps-main/src/__tests__/exercise/01.js
@@ -1,35 +1,54 @@
-// simple test with ReactDOM
-// http://localhost:3000/counter
-
 import * as React from 'react'
+import ReactDOM from 'react-dom';
 import {act} from 'react-dom/test-utils'
 import {createRoot} from 'react-dom/client'
 import Counter from '../../components/counter'
 
-// NOTE: this is a new requirement in React 18
-// https://react.dev/blog/2022/03/08/react-18-upgrade-guide#configuring-your-testing-environment
-// Luckily, it's handled for you by React Testing Library :)
 global.IS_REACT_ACT_ENVIRONMENT = true
 
-test('counter increments and decrements when the buttons are clicked', () => {
-  // 🐨 create a div to render your component to (💰 document.createElement)
-  //
-  // 🐨 append the div to document.body (💰 document.body.append)
-  //
-  // 🐨 use createRoot to render the <Counter /> to the div
-  // 🐨 get a reference to the increment and decrement buttons:
-  //   💰 div.querySelectorAll('button')
-  // 🐨 get a reference to the message div:
-  //   💰 div.firstChild.querySelector('div')
-  //
-  // 🐨 expect the message.textContent toBe 'Current count: 0'
-  // 🐨 click the increment button (💰 act(() => increment.click()))
-  // 🐨 assert the message.textContent
-  // 🐨 click the decrement button (💰 act(() => decrement.click()))
-  // 🐨 assert the message.textContent
-  //
-  // 🐨 cleanup by removing the div from the page (💰 div.remove())
-  // 🦉 If you don't cleanup, then it could impact other tests and/or cause a memory leak
+beforeEach(() => {
+  // Cleans up DOM before each test to ensure there are no unwanted side effects
+  document.body.innerHTML = "";
 })
+
+test('counter increments and decrements when the buttons are clicked', () => {
+  
+  // To begin, a basic DOM is created that contains the Counter component.
+  // This is then rendered and tested to see if it contains the state we expect it to.
+  const div = document.createElement("div");
+  document.body.append(div);
+
+  const root = createRoot(div);
+
+  // Act is used to change the state on the DOM.
+  act(() => {
+    root.render(<Counter />)
+  });
+
+  // Using the MouseEvent follows a users experience a little closer [0 = left button click]
+  const clickEvent = new MouseEvent("click", {
+    bubbles: true,
+    cancelable: true,
+    button: 0,
+  });
+ 
+  // Testing state uploads correctly, should always be 0
+  // Testing buttons
+  const msg = div.firstChild.querySelector("div");
+  expect(msg.textContent).toBe("Current count: 0");
+
+  const [decrementButton, incrementButton] = div.querySelectorAll("button");
+
+  act(() => {
+    incrementButton.dispatchEvent(clickEvent);
+  });
+  expect(msg.textContent).toBe("Current count: 1");
+
+  act(() => {
+    decrementButton.dispatchEvent(clickEvent);
+    decrementButton.dispatchEvent(clickEvent);
+  });
+  expect(msg.textContent).toBe("Current count: -1");
+});
 
 /* eslint no-unused-vars:0 */
