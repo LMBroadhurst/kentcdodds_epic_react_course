f510e50a98f82481b2a529f8b76ada8b28019a0e
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2023-08-25 00:29:18 +0100

    start ex5

--- testing-react-apps-main/src/__tests__/exercise/04.js
+++ testing-react-apps-main/src/__tests__/exercise/04.js
@@ -2,16 +2,34 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
+import { build, fake } from "@jackfranklin/test-data-bot"
+
+const buildLoginForm = build({
+  fields: {
+    username: fake(f => f.internet.userName()),
+    password: fake(f => f.internet.password()),
+  }
+})
+
+// Generate random userName and password
+// function buildLoginForm(overrides) {
+//   return {
+//     username: faker.internet.userName(),
+//     password: faker.internet.password(),
+//     // overrides allows for additional properties or property overrides
+//     ...overrides
+//   }
+// }
 
 test('submitting the form calls onSubmit with username and password', async () => {
   const user = userEvent.setup()
 
-  let submittedData;
-  const handleSubmit = (data) => (submittedData = data)
+  // let submittedData;
+  // const handleSubmit = (data) => (submittedData = data)
+  // Instead of mocking a function & creating submittedData...
+  const handleSubmit = jest.fn();
   render(<Login onSubmit={handleSubmit} />)
-
-  const username = "Just my name";
-  const password = "No password";
+  const { username, password } = buildLoginForm();
 
   const usernameInput = screen.getByLabelText(/username/i);
   const passwordInput = screen.getByLabelText(/password/i);
@@ -21,5 +39,6 @@ test('submitting the form calls onSubmit with username and password', async () =
   await user.type(passwordInput, password)
   await user.click(submitButton)
 
-  expect(submittedData).toEqual({ username, password })
+  expect(handleSubmit).toBeCalledWith({ username, password });
+  expect(handleSubmit).toHaveBeenCalledTimes(1);
 })
