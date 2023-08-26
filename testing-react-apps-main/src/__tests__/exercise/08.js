46e6551c3ce73b27da0feb3fccda17e55bc8a3b0
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2023-08-26 14:13:18 +0100

    fin 7

--- testing-react-apps-main/src/__tests__/exercise/08.js
+++ testing-react-apps-main/src/__tests__/exercise/08.js
@@ -2,21 +2,101 @@
 // http://localhost:3000/counter-hook
 
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
+import {render, screen, act} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import useCounter from '../../components/use-counter'
 
-// 🐨 create a simple function component that uses the useCounter hook
-// and then exposes some UI that our test can interact with to test the
-// capabilities of this hook
-// 💰 here's how to use the hook:
-// const {count, increment, decrement} = useCounter()
+function UseCounterHookExample() {
+  const {count, increment, decrement} = useCounter();
 
-test('exposes the count and increment/decrement functions', () => {
-  // 🐨 render the component
-  // 🐨 get the elements you need using screen
-  // 🐨 assert on the initial state of the hook
-  // 🐨 interact with the UI using userEvent and assert on the changes in the UI
+  return (
+    <div>
+      <span>Current Count: {count}</span>
+      <button onClick={increment}>plus</button>
+      <button onClick={decrement}>minus</button>
+    </div>
+  )
+}
+
+function setup({initialProps} = {}) {
+  const result = {};
+  function TestComponent() {
+    result.current = useCounter(initialProps)
+    return null;
+  }
+  render(<TestComponent />)
+  return result;
+}
+
+
+test('exposes the count and increment/decrement functions', async () => {
+  const user = userEvent.setup();
+  render(<UseCounterHookExample />);
+  
+  const countMessage = screen.getByText(/current count/i);
+  const increment = screen.getByRole("button", {name: /plus/i})
+  const decrement = screen.getByRole("button", {name: /minus/i})
+  
+  expect(countMessage).toHaveTextContent(/current count: 0/i)
+
+  await user.click(increment);
+  await user.click(increment);
+  await user.click(increment);
+  expect(countMessage).toHaveTextContent(/current count: 3/i)
+
+  await user.click(decrement);
+  await user.click(decrement);
+  expect(countMessage).toHaveTextContent(/current count: 1/i)
 })
 
+
+test('exposes the count and increment/decrement functions for complicated components', async () => {
+
+  const result = setup();
+
+  expect(result.current.count).toBe(0);
+
+  act(() => {result.current.increment()})
+  expect(result.current.count).toBe(1);
+
+  act(() => {
+    result.current.decrement();
+    result.current.decrement();
+  })
+  expect(result.current.count).toBe(-1);
+})
+
+
+test('test initial count state', async () => {
+
+  const result = setup({initialProps: {initialCount: 3}});
+
+  expect(result.current.count).toBe(3);
+
+  act(() => {result.current.increment()})
+  expect(result.current.count).toBe(4);
+
+  act(() => {
+    result.current.decrement();
+    result.current.decrement();
+  })
+  expect(result.current.count).toBe(2);
+})
+
+
+test('test step', async () => {
+
+  const result = setup({initialProps: {initialCount: 3, step: 2}});
+
+  expect(result.current.count).toBe(3);
+
+  act(() => {result.current.increment()})
+  expect(result.current.count).toBe(5);
+
+  act(() => {
+    result.current.decrement();
+    result.current.decrement();
+  })
+  expect(result.current.count).toBe(1);
+})
 /* eslint no-unused-vars:0 */
