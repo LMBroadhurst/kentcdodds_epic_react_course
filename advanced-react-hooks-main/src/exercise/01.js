439b0f1bb93ff3808d1824b3f62641900620230b
Lewis <lewis.broadhurst@mahimarkets.com>
2023-08-29 16:55:22 +0100

    fin exercise 1

--- advanced-react-hooks-main/src/exercise/01.js
+++ advanced-react-hooks-main/src/exercise/01.js
@@ -1,18 +1,29 @@
-// useReducer: simple Counter
-// http://localhost:3000/isolated/exercise/01.js
-
 import * as React from 'react'
 
-function Counter({initialCount = 0, step = 1}) {
-  // 🐨 replace React.useState with React.useReducer.
-  // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useState(initialCount)
+// Reducers accepts two arguments, the current state and the action you are trying to perform
+const countReducer = (state, action) => {
+  const step = action.payload.step;
+  switch (action.type) {
+    case "INCREMENT_COUNT":
+      return {...state, count: state.count + step};
 
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step)
+    default:
+      throw new Error(`Unsupported action type: ${action.type}`);
+  }
+}
+
+function Counter({initialCount = 0, step = 3}) {
+
+  // Think about useState and useReducer like this...
+  // dispatch... "updates the state to a different value and triggers a re-render" -- "has no return value"
+  const [state, dispatch] = React.useReducer(countReducer, {count: initialCount});
+
+  const {count} = state;
+
+  // Q: Why don't we send state in the dispatch? 
+  // Can get swifty with the payload now... Cracked the code a little here :D
+  const increment = () => dispatch({type: "INCREMENT_COUNT", payload: {step: step}});
+
   return <button onClick={increment}>{count}</button>
 }
 
